bcbdefd34d2e15b806adf20a9381b40feb743015
NganNLK <39646963+NganNLK@users.noreply.github.com>
2021-02-25 17:39:35 +0700

    add verify URL test case

--- src/topic1.java
+++ src/topic1.java
@@ -1,8 +1,31 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
 public class topic1 {
-    public static void main (String[] args){
-        int a = 10;
-        int b = 11;
+    WebDriver driver;
 
+    @BeforeClass
+    public void beforeClass() {
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("http://demo.guru99.com/v4");
+    }
 
+    @Test
+    public void validateURL() {
+        String loginUrl = driver.getCurrentUrl();
+        Assert.assertEquals(loginUrl, "http://demo.guru99.com/v4");
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
     }
 }
